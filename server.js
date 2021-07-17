a42e7488705382f8236b7daf7fcce2d8a4e6c378
EC2 Default User <ec2-user@ip-172-31-78-228.ec2.internal>
2021-07-17 19:03:48 +0000

    added all files

---
+++ server.js
@@ -0,0 +1,14 @@
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function main() {
+  const favoriteFood = process.env.FAVORITE_FOOD;
+  console.log(`My favorite food is ${favoriteFood}`);
+  while (true) {
+    console.log("Microservices rock!");
+    await sleep(5000);
+  }
+}
+
+main();
